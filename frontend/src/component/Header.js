7e1e42aadec6587b3897422dfdacca95498626c4
ihor-vip <vip20140902@gmail.com>
2023-06-21 11:56:31 +0300

    create dropdown menu for user header section

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
-import {FaUserAlt} from "react-icons/fa";
 import {BsCartFill} from "react-icons/bs";
+import {HiOutlineUserCircle} from "react-icons/hi";
 
 import logo from '../assets/logo.png'
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleShowMenu = () => {
+        setShowMenu(prev => !prev)
+    }
+
     return (
         <header className='fixed shadow-md w-full h-16 px-2 md:px-4 '>
             <div className='flex items-center h-full justify-between'>
@@ -23,12 +29,25 @@ const Header = () => {
                         <Link to={'contact'}>Contact</Link>
                     </nav>
 
-                    <div className='text-2xl text-slate-600'>
+                    <div className='text-2xl text-slate-600 relative'>
                         <BsCartFill/>
+                        <div className='absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center'>
+                            0
+                        </div>
                     </div>
 
-                    <div className='text-2xl text-slate-600'>
-                        <FaUserAlt/>
+                    <div className='text-slate-600' onClick={handleShowMenu}>
+                        <div className='text-3xl cursor-pointer'>
+                            <HiOutlineUserCircle/>
+                        </div>
+
+                        {
+                            showMenu && ( <div className='absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md'>
+                                             <p className='whitespace-nowrap cursor-pointer'>New product</p>
+                                             <p className='whitespace-nowrap cursor-pointer'>Login</p>
+                                          </div>
+                        )}
+
                     </div>
                 </div>
             </div>
