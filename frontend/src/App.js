46455d1b0dd55e53c14eb3b7a079809bf331ea12
ihor-vip <vip20140902@gmail.com>
2023-06-28 15:41:22 +0300

    create redux product slice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,33 @@
 import {Outlet} from "react-router-dom";
-import toast, {Toast, Toaster} from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
 
 import Header from "./component/Header";
+import {setDataProduct} from "./redux/productSlice";
 
 function App() {
-  return (
-    <>
-        <Toaster />
-        <div>
-            <Header />
-            <main className='p-16 bg-slate-100 min-h-[calc(100vh)]'>
-                <Outlet/>
-            </main>
-        </div>
-    </>
-  );
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        (async() => {
+            const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`)
+            const resData = await res.json()
+            dispatch(setDataProduct(resData))
+        })()
+    },[])
+
+    return (
+        <>
+            <Toaster />
+            <div>
+                <Header />
+                <main className='p-16 bg-slate-100 min-h-[calc(100vh)]'>
+                    <Outlet/>
+                </main>
+            </div>
+        </>
+      );
 }
 
 export default App;
