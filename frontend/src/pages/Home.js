e63017b3e62c3c2e09f941bcbe86508502cb33e8
ihor-vip <vip20140902@gmail.com>
2023-06-29 16:22:22 +0300

    home page complete

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,26 @@
-import React from "react";
-import {useSelector} from "react-redux";
-
-import HomeCard from "../component/HomeCard";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import CardFeature from "../component/CardFeature";
+import HomeCard from "../component/HomeCard";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import FilterProduct from "../component/FilterProduct";
+import AllProduct from "../component/AllProduct";
+
 
 const Home = () => {
     const productData = useSelector((state) => state.product.productList);
     const homeProductCartList = productData.slice(1, 5);
-    const homeProductCartListVegetables = productData.filter((el) => el.category === "vegetable", []);
+    const homeProductCartListVegetables = productData.filter((el) => el.category === 'vegetable', []);
     const loadingArray = new Array(4).fill(null);
     const loadingArrayFeature = new Array(10).fill(null);
+
+    const slideProductRef = useRef();
+    const nextProduct = () => {
+        slideProductRef.current.scrollLeft += 200;
+    };
+    const prevProduct = () => {
+        slideProductRef.current.scrollLeft -= 200;
+    };
 
     return (
         <div className='p-2 md:p-4'>
@@ -23,7 +34,7 @@ const Home = () => {
                         />
                     </div>
                     <h2 className='text-4xl md:text-7xl font-bold py-3'>
-                        The Fastest Delivery in{' '}
+                        The Fasted Delivery in{' '}
                         <span className='text-red-600 text-'>Your Home</span>
                     </h2>
                     <p className='py-3 text-base'>
@@ -53,30 +64,55 @@ const Home = () => {
                             );
                         })
                         : loadingArray.map((el, index) => {
-                            return <HomeCard key={index + 'loading'} loading={'Loading...'} />;
+                            return <HomeCard key={index+'loading'} loading={'Loading...'}/>;
                         })}
                 </div>
             </div>
 
-            <div className=''>
-                <h2 className='font-bold text-2xl text-slate-800'>Fresh Vegetables</h2>
-                <div className=''>
-                    {
-                        homeProductCartListVegetables.map(el => {
+            <div>
+                <div className='flex w-full items-center'>
+                    <h2 className='font-bold text-2xl text-slate-800 mb-4'>
+                        Fresh Vegetables
+                    </h2>
+                    <div className='ml-auto flex gap-4'>
+                        <button
+                            onClick={prevProduct}
+                            className='bg-slate-300 hover:bg-slate-400 text-lg  p-1 rounded'
+                        >
+                            <GrPrevious />
+                        </button>
+                        <button
+                            onClick={nextProduct}
+                            className='bg-slate-300 hover:bg-slate-400 text-lg p-1 rounded'
+                        >
+                            <GrNext />
+                        </button>
+                    </div>
+                </div>
+                <div
+                    className='flex gap-5 overflow-scroll scrollbar-none scroll-smooth transition-all'
+                    ref={slideProductRef}
+                >
+                    {homeProductCartListVegetables[0]
+                        ? homeProductCartListVegetables.map((el) => {
                             return (
                                 <CardFeature
-                                    key={el._id + 'vegetable'}
+                                    key={el._id+'vegetable'}
                                     id={el._id}
                                     name={el.name}
                                     category={el.category}
                                     price={el.price}
                                     image={el.image}
                                 />
-                            )
+                            );
                         })
-                    }
+                        : loadingArrayFeature.map((el,index) => (
+                            <CardFeature loading='Loading...' key={index+'cartLoading'} />
+                        ))}
                 </div>
             </div>
+
+            <AllProduct heading={'Your Product'}/>
         </div>
     );
 };
