9b79a798806833d44e08f377040ea222730f0e13
ihor-vip <vip20140902@gmail.com>
2023-06-29 09:36:11 +0300

    create HomeCard component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import {useSelector} from "react-redux";
+
+import HomeCard from "../component/HomeCard";
+
 const Home = () => {
+    const productData = useSelector((state) => state.product.productList);
+    const homeProductCartList = productData.slice(1, 5);
+    const loadingArray = new Array(4).fill(null);
+
     return (
         <div className='p-2 md:p-4'>
             <div className='md:flex gap-4 py-2'>
@@ -27,10 +35,23 @@ const Home = () => {
                     </button>
                 </div>
 
-                <div className="md:w-1/2 flex flex-wrap gap-5 p-4 justify-center">
-                    <div>image</div>
-                    <div>image</div>
-                    <div>image</div>
+                <div className='md:w-1/2 flex flex-wrap gap-5 p-4 justify-center'>
+                    {homeProductCartList[0]
+                        ? homeProductCartList.map((el) => {
+                            return (
+                                <HomeCard
+                                    key={el._id}
+                                    id={el._id}
+                                    image={el.image}
+                                    name={el.name}
+                                    price={el.price}
+                                    category={el.category}
+                                />
+                            );
+                        })
+                        : loadingArray.map((el, index) => {
+                            return <HomeCard key={index+'loading'} loading={'Loading...'} />;
+                        })}
                 </div>
             </div>
         </div>
